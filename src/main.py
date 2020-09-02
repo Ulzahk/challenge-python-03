91131a7e79484d004b3a04ad086971a030460bae
Ulzahk <ulzahk@gmail.com>
2020-09-02 09:27:05 -0500

    Challenge's answer using open().readlines(), re.compile() and re.findall()

--- src/main.py
+++ src/main.py
@@ -1,8 +1,24 @@
-# Resolve the problem!!
-
+import re
 
 def run():
-    # Start coding here
+    file_path = 'src/encoded.txt'
+    regex = '[a-z]+'
+    pattern = re.compile(regex)
+    secret_message = ''
+
+    with open(file_path, 'r', encoding='utf-8') as f:
+        
+        for line in f.readlines():
+            secret_message += ''.join(re.findall(pattern, line))
+
+        # Other way to get the secret message, remember to comment the other loop to make this one work
+        """ for i, line in enumerate(f):
+            for match in re.finditer(pattern, line):
+                secret_message += match.group() """
+
+    f.close()
+    
+    print(secret_message)
 
 
 if __name__ == '__main__':
